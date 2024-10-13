d97349f8c6537c9d26e6286a6c9e68ae6ee6dd2d
Yuka <yoz.peridot.738@gmail.com>
2024-10-14 00:51:03 +0900

    メール画面作成

--- App.tsx
+++ App.tsx
@@ -1,20 +1,27 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import Question1 from './app/question/question1';
 
-export default function App() {
+const App = () => {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app aaaaa bbbbb ccccc</Text>
-      <StatusBar style="auto" />
+    // <View style={styles.container}>
+    <View>
+      <Question1 />  
+    
     </View>
+
+    // <StatusBar style="auto" />
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+// const styles = StyleSheet.create({
+//   container: {
+//     flex: 1,
+//     backgroundColor: '#fff',
+//     alignItems: 'center',
+//     justifyContent: 'center',
+//   },
+// });
+
+
+export default App
