33e959cdd88b5edbc85ded0dae84c2c50ef92160
Kohei-Takano <a8121153@gsuite.si.aoyama.ac.jp>
2024-10-28 12:49:27 +0900

    developブランチの状態へ修正

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -4,12 +4,7 @@ import React from "react";
 import { Redirect } from "expo-router";
 
 const Index = () => {
-<<<<<<< HEAD
-    return <Redirect href='App' />
-}
-=======
   return <Redirect href="goji" />;
 };
->>>>>>> b0dd221a8cf87c9ee4f867f6f8bf3f64475cfaf4
 
 export default Index;
