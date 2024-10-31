ccea38dbfa3c9eb1194197baa5856ec6016a4f8e
pasta1207 <yucie1207@gmail.com>
2024-10-31 20:17:06 +0900

    リンクを修正

--- src/app/home/index.tsx
+++ src/app/home/index.tsx
@@ -8,10 +8,10 @@ const Home = () => {
   return (
     <>
       <Text>Homeページ</Text>
-      <Link href="./question/goji">問題ページ1</Link>
-      <Link href="./question/nottori">問題ページ2</Link>
-      <Link href="./question/question1">問題ページ3</Link>
-      <Link href="./question/question2">問題ページ4</Link>
+      <Link href="home/question/goji">問題ページ1</Link>
+      <Link href="home/question/nottori">問題ページ2</Link>
+      <Link href="home/question/question1">問題ページ3</Link>
+      <Link href="home/question/question2">問題ページ4</Link>
     </>
   );
 };
