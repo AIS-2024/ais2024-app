ee05574078d9dd51b9d3df07a69d41982c0ff612
a8121160 <gagamhw333@gmail.com>
2025-01-04 18:45:43 +0900

    ログアウトボタンの修正

--- src/components/LogOutButton.tsx
+++ src/components/LogOutButton.tsx
@@ -1,39 +1,39 @@
 import React, { useState } from "react";
-import { TouchableOpacity,StyleSheet,Text, Alert, ActivityIndicator, Pressable, View } from "react-native";
+import { TouchableOpacity, StyleSheet, Text, Alert, ActivityIndicator, Pressable, View } from "react-native";
 import { signOut } from "firebase/auth";
 import { router } from "expo-router";
 import { auth } from "../config";
 
-const LogOutButton=()=>{
+const LogOutButton = () => {
 
-    const handlePress=()=>{
-        signOut(auth).then(()=>{
+    const handlePress = () => {
+        signOut(auth).then(() => {
             router.replace("/");
-        }).catch((error)=>{
+        }).catch((error) => {
             Alert.alert('ログアウトに失敗しました')
         });
-                
+
     }
 
-    return(
+    return (
         <TouchableOpacity onPressIn={handlePress} style={styles.logoutButton}>
             <Text style={styles.text}>ログアウト</Text>
         </TouchableOpacity>
     )
 }
 
-const styles=StyleSheet.create({
+const styles = StyleSheet.create({
     logoutButton: {
         marginRight: 10, // ヘッダー右端からの余白
-        padding: 10,
+        padding: 5,
         borderRadius: 5,
-        backgroundColor:'orange',
+        backgroundColor: 'orange',
         zIndex: 10,
-      },
-    text:{
-        fontSize:12,
-        lineHeight:24,
-        color:'#000000'
+    },
+    text: {
+        fontSize: 12,
+        lineHeight: 24,
+        color: '#000000'
     }
 })
 
